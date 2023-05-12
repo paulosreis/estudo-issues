166b39eda813d8eb643c741f300f7eb4baa9e5ac
RAFAEL0PENHA <r.penha@ufms.br>
2023-05-12 18:51:20 -0300

    cria variavel

--- commit.cpp
+++ commit.cpp
@@ -2,17 +2,9 @@
 
 
 int main () {
-<<<<<<< HEAD
     int x1;
 
     x1 = 0;
     printf("modificando");
     printf("%d\n", x1);
-=======
-    int x2;
-
-    x2 = 0;
-    printf("modificando");
-    printf("%d\n", x2);
->>>>>>> d99182438ff1a4fc8918c715d750afeaa2f40131
 }
